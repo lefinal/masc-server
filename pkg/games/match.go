75424e0f13d4e4d3e62943a15f9a69f1c82eb5c6
Lennart Altenhof <l.f.altenhof@gmail.com>
2020-09-29 02:19:33 +0200

    Removed old match config attributes

--- pkg/games/match.go
+++ pkg/games/match.go
@@ -2,14 +2,6 @@ package games
 
 import "github.com/google/uuid"
 
-type MatchAttributeKey string
-
-const (
-	TeamCountMatchAttribute   MatchAttributeKey = "team-count"
-	PlayerCountMatchAttribute MatchAttributeKey = "player-count"
-	LifeCountMatchAttribute   MatchAttributeKey = "life-count"
-)
-
 type Match struct {
 	Id   uuid.UUID `json:"id"`
 	Mode GameMode  `json:"mode"`
